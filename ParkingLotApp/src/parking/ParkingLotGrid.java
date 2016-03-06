ce015976f684d2af6cf275e446988c90085f9594
Mcelhennyi <mcelhenny_ian@yahoo.com>
2016-03-06 18:09:12 -0500

    Added tested values to spot 0-10 for HSV filtering

--- ParkingLotApp/src/parking/ParkingLotGrid.java
+++ ParkingLotApp/src/parking/ParkingLotGrid.java
@@ -18,14 +18,14 @@ public class ParkingLotGrid
 	private int totalSpots = 28; //Starting count with 1 not zero.
 
 	//Hard Coded array of Ranges for all the spots, if we make it were this is done through the GUI this isnt needed anymore
-	private Range[] xRange = {  new Range(195, 218), new Range(224, 258),new Range(262, 301),new Range(299, 341),new Range(373, 422),new Range(414, 455),
-								new Range(447, 493), new Range(484, 528),new Range(520, 558),new Range(191, 232),new Range(237, 281),new Range(279, 328),
+	private Range[] xRange = {  new Range(195, 218), new Range(224, 258),new Range(262, 301),new Range(299, 341),new Range(387, 412),new Range(425, 455),
+								new Range(455, 487), new Range(484, 528),new Range(520, 550),new Range(191, 232),new Range(250, 275),new Range(279, 328),
 								new Range(319, 370), new Range(360, 415),new Range(402, 458),new Range(433, 501),new Range(477, 538),new Range(513, 570),
 								new Range(544, 602), new Range(581, 634),new Range(608, 659),new Range(635, 690),new Range(107, 157),new Range(173, 222),
 								new Range(228, 286), new Range(283, 350),new Range(339, 412),new Range(394, 471)};
 	
 	private Range[] yRange = {  new Range(220, 259), new Range(229, 260),new Range(227, 262),new Range(230, 264),new Range(233, 266),new Range(233, 267),
-								new Range(235, 270), new Range(240, 269),new Range(243, 273),new Range(276, 328),new Range(278, 329),new Range(282, 333),
+								new Range(235, 270), new Range(240, 269),new Range(243, 273),new Range(276, 315),new Range(282, 329),new Range(282, 333),
 								new Range(284, 334), new Range(284, 333),new Range(285, 335),new Range(285, 334),new Range(289, 334),new Range(291, 334),
 								new Range(289, 335), new Range(293, 335),new Range(292, 330),new Range(291, 330),new Range(401, 480),new Range(408, 478),
 								new Range(415, 478), new Range(413, 479),new Range(403, 479),new Range(395, 478)};
@@ -34,6 +34,23 @@ public class ParkingLotGrid
 	private Scalar spot0_8 = new Scalar(170, 62, 245);
 	private Scalar spot9_27 = new Scalar(120,45,170);
 
+	//Copied values starting at 1 :6->7,8,9,10
+	private Scalar[] lowerArray = {
+			new Scalar(0,0,90), new Scalar(0,0,105), new Scalar(0,0,105), new Scalar(0,0,105), new Scalar(0,0,110), new Scalar(0,0,70), 
+			new Scalar(0,0,70), new Scalar(0,0,70), new Scalar(0,0,70), new Scalar(0,0,70), new Scalar(0,0,50), new Scalar(0,0,0), 
+			new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), 
+			new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), 
+			new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0)};
+	
+	//Copied values starting at 1 :6->7,8,9,10
+	private Scalar[] upperArray = {
+			new Scalar(90,90,130), new Scalar(150,7,140), new Scalar(140,10,130), new Scalar(150,15,130), new Scalar(150,20,125), new Scalar(140,20,130), 
+			new Scalar(140,20,130), new Scalar(140,20,130), new Scalar(140,20,130), new Scalar(140,20,130), new Scalar(150,30,85), new Scalar(0,0,0), 
+			new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), 
+			new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), 
+			new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0), new Scalar(0,0,0)};
+
+	
 	
 	public ParkingLotGrid()
 	{
@@ -62,8 +79,18 @@ public class ParkingLotGrid
 		{
 			this.myGrid[i].setLocation(xRange[i], yRange[i]);
 		}
-		setHSV();
+		setHSVIndividual();
 	}
+	
+	//Gives a certain number spot a location of pixels in the picture for all spots in the photo
+		public void setHSVIndividual()
+		{
+			for(int i = 0; i <= this.myGrid.length - 1; i++)
+			{
+				this.myGrid[i].setHSVLimits(this.lowerArray[i], this.upperArray[i]);
+			}
+		}
+	
 	
 	//Set the hsv value for each spot
 	public void setHSV()
